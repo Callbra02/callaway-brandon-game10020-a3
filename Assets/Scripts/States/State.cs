fe99ef056eda430dc514d4f54b51cc3d19d6503b
000954560@mohawkcollege.ca <brandon.callaway.29@gmail.com>
2026-03-29 16:53:33 -0400

    Create basic polymorphic state machine

---
+++ Assets/Scripts/States/State.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public abstract class State : MonoBehaviour
+{
+    public StateMachine stateMachine;
+
+    protected State(StateMachine _stateMachine)
+    {
+        stateMachine = _stateMachine;
+    }
+
+    public abstract void Enter();
+    public abstract void Execute();
+    public abstract void Exit();
+}
